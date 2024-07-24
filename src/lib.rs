3927e3002b66de3926ce04b28c94e18a913316f5
Ian Laidlaw <ian@kaboodles.com>
2024-07-24 18:11:36 -0400

    Temporarily disabled codegen

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,15 @@ extern crate core;
 
 pub use assembly::*;
 pub use ast::*;
-pub use codegen::*;
+//pub use codegen::*;
 pub use lexer::*;
 pub use parser::*;
 
 pub mod assembly;
 pub mod ast;
-pub mod codegen;
+//pub mod codegen;
 pub mod lexer;
 pub mod parser;
 pub mod tacky;
 pub mod tacky_gen;
+pub mod assembly_gen;
