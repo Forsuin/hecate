43f9cdc876f1293f852186459625fb56a3a84ad9
Ian Laidlaw <ian@kaboodles.com>
2024-07-24 19:15:16 -0400

    Derived Clone for all nodes

--- src/assembly.rs
+++ src/assembly.rs
@@ -1,17 +1,17 @@
 /// Defines assembly tree datatypes
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Program {
     pub func: Function,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Function {
     pub name: String,
     pub instructions: Vec<Instruction>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Instruction {
     Mov { src: Operand, dest: Operand },
     Unary{ op: UnaryOp, dest: Operand },
@@ -19,13 +19,13 @@ pub enum Instruction {
     Ret,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum UnaryOp {
     Neg,
     Not,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Operand {
     Imm(i32),
     Register(Register),
@@ -33,7 +33,7 @@ pub enum Operand {
     Stack(i32),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Register {
     AX,
     R10,
